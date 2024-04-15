e1531c907d3f86145f210ed5292536943020edfa
Ihsanuddin18 <mihsan18@gmail.com>
2024-04-16 01:45:34 +0700

    Change-mb password

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -14,20 +14,20 @@
         @method('put')
 
         <div>
-            <x-input-label for="update_password_current_password" :value="__('Current Password')" />
-            <x-text-input id="update_password_current_password" name="current_password" type="password" class="form-control" autocomplete="current-password" />
+            <x-input-label for="update_password_current_password" :value="__('Current Password')" class="mb-3"/>
+            <x-text-input id="update_password_current_password" name="current_password" type="password" class="form-control mb-3" autocomplete="current-password" />
             <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
         </div>
 
         <div>
-            <x-input-label for="update_password_password" :value="__('New Password')" />
-            <x-text-input id="update_password_password" name="password" type="password" class="form-control" autocomplete="new-password" />
+            <x-input-label for="update_password_password" :value="__('New Password')" class="mb-3"/>
+            <x-text-input id="update_password_password" name="password" type="password" class="form-control mb-3" autocomplete="new-password" />
             <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
         </div>
 
         <div>
-            <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password" class="form-control" autocomplete="new-password" />
+            <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" class="mb-3"/>
+            <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password" class="form-control mb-3" autocomplete="new-password" />
             <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
         </div>
 
