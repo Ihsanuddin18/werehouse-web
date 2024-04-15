e86bf73583fdb77a501585422ea63b49b7678ad3
Ihsanuddin18 <mihsan18@gmail.com>
2024-04-15 23:52:54 +0700

    profile-change

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,6 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 
-// Route tampilan features-profile
-Route::get('/features-profile', [FeaturesProfileController::class, 'show'])->name('features-profile');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
