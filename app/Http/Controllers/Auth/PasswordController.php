b505fa348f6efe0be77cd85b0f80e0b2897bb468
Ihsanuddin18 <mihsan18@gmail.com>
2024-04-19 10:55:57 +0700

    Edit-password controller

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
-use Illuminate\Support\Facades\Validator;
 
 class PasswordController extends Controller
 {
@@ -16,25 +15,11 @@ class PasswordController extends Controller
      */
     public function update(Request $request): RedirectResponse
     {
-        $validated = $request->validate([
+        $validated = $request->validateWithBag('updatePassword', [
             'current_password' => ['required', 'current_password'],
             'password' => ['required', Password::defaults(), 'confirmed'],
         ]);
-
-        // Custom validation for empty fields
-        if (empty($request->current_password) || empty($request->password)) {
-            return back()->withErrors(['message' => 'Kolom password tidak boleh kosong!']);
-        }
-
-        // Validate again with the same rules
-        $validator = Validator::make($request->all(), [
-            'current_password' => ['required', 'current_password'],
-            'password' => ['required', Password::defaults(), 'confirmed'],
-        ]);
-
-        if ($validator->fails()) {
-            return back()->withErrors($validator);
-        }
+        
 
         $request->user()->update([
             'password' => Hash::make($validated['password']),
