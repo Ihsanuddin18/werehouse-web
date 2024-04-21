e8d75de68801b742376804e6849d3bf5d008aa85
Ihsanuddin18 <mihsan18@gmail.com>
2024-04-21 20:33:18 +0700

    edit-breadcrumb

--- resources/views/tambah_akun.blade.php
+++ resources/views/tambah_akun.blade.php
@@ -397,11 +397,11 @@
       <div class="main-content">
         <section class="section">
           <div class="section-header">
-            <h1>Default Layout</h1>
+            <h1>Admin</h1>
             <div class="section-header-breadcrumb">
-              <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-              <div class="breadcrumb-item"><a href="#">Layout</a></div>
-              <div class="breadcrumb-item">Default Layout</div>
+              <div class="breadcrumb-item active"><a href="{{ route('dashboard') }}">Dashboard</a></div>
+              <div class="breadcrumb-item"><a href="{{ route('tambah_akun') }}">Tambah Akun</a></div>
+              <div class="breadcrumb-item">Admin</div>
             </div>
           </div>
 
