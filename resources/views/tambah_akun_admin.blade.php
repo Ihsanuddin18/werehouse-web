7a51759b4e7280680dc4f976d68ce22eba52c549
Ihsanuddin18 <mihsan18@gmail.com>
2024-04-23 09:46:08 +0700

    Tambah akun admin: penambahan button dan status

--- resources/views/tambah_akun_admin.blade.php
+++ resources/views/tambah_akun_admin.blade.php
@@ -452,44 +452,42 @@
             <div class="col-md-8">
               <div class="card">
                 <div class="card-header">
-                  <h4>Invoices</h4>
-                  <div class="card-header-action">
-                    <a href="#" class="btn btn-danger">View More <i class="fas fa-chevron-right"></i></a>
-                  </div>
+                  <h4>Data Admin</h4>
+                  
                 </div>
                 <div class="card-body p-0">
                   <div class="table-responsive table-invoice">
                     <table class="table table-striped">
                       <tr>
-                        <th>Invoice ID</th>
-                        <th>Customer</th>
-                        <th>Status</th>
-                        <th>Due Date</th>
-                        <th>Action</th>
+                        <th>ID </th>
+                        <th>NAMA</th>
+                        <th>EMAIL</th>
+                        <th>STATUS</th>
+                        <th>AKSI</th>
                       </tr>
                       <tr>
                         <td><a href="#">INV-87239</a></td>
                         <td class="font-weight-600">Kusnadi</td>
-                        <td><div class="badge badge-warning">Unpaid</div></td>
                         <td>July 19, 2018</td>
-                        <td>
-                          <a href="#" class="btn btn-primary">Detail</a>
+                        <td><div class="badge badge-warning">Unpaid</div></td>
+                        <td> 
+                          <a href="#" class="btn btn-icon btn-danger"><i class="fas fa-times"></i></a>
                         </td>
                       </tr>
                       <tr>
                         <td><a href="#">INV-48574</a></td>
                         <td class="font-weight-600">Hasan Basri</td>
-                        <td><div class="badge badge-success">Paid</div></td>
                         <td>July 21, 2018</td>
+                        <td><div class="badge badge-success">Paid</div></td>
                         <td>
-                          <a href="#" class="btn btn-primary">Detail</a>
+                        <a href="#" class="btn btn-icon btn-danger"><i class="fas fa-times"></i></a>
                         </td>
                       </tr>
                       <tr>
                         <td><a href="#">INV-76824</a></td>
                         <td class="font-weight-600">Muhamad Nuruzzaki</td>
-                        <td><div class="badge badge-warning">Unpaid</div></td>
                         <td>July 22, 2018</td>
+                        <td><div class="badge badge-warning">Unpaid</div></td>
                         <td>
                           <a href="#" class="btn btn-primary">Detail</a>
                         </td>
