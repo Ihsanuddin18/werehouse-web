da5e514c1e3303a9ca368e97e1c8b506a69694ea
Ihsanuddin18 <mihsan18@gmail.com>
2024-04-24 21:20:11 +0700

    Tambah akun anggota: edit tampilan

--- resources/views/tambah_akun_anggota.blade.php
+++ resources/views/tambah_akun_anggota.blade.php
@@ -446,7 +446,6 @@
           <div class="buttons">
             <a href="{{ route('tambah_akun_anggota') }}" class="btn btn-icon icon-left"><i class="far fa-user"></i> Anggota</a>
           </div>
-
           <div class="row">
             <div class="col-md-8">
               <div class="card">
@@ -538,78 +537,78 @@
                 </div>
               </div>
             </div>
-            <div class="col-md-4">
-        <button class="btn btn-primary" data-toggle="modal" data-target="#tambahModal">Tambah</button>
-    </div>
-</div>
-<div class="modal fade" id="tambahModal" tabindex="-1" aria-labelledby="tambahModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <form class="needs-validation" novalidate="">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="tambahModalLabel">Form Tambah Anggota</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="form-group row">
-                        <label for="nama" class="col-sm-3 col-form-label">Nama</label>
-                        <div class="col-sm-9">
-                            <input type="text" class="form-control" id="nama" name="nama" required>
-                            <div class="invalid-feedback">
-                                Kolom wajib diisi !
-                            </div>
-                            <div class="valid-feedback">
-                                Nama valid !
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="email" class="col-sm-3 col-form-label">Email</label>
-                        <div class="col-sm-9">
-                            <input type="email" class="form-control" id="email" name="email" required>
-                            <div class="invalid-feedback">
-                                Email tidak valid !
-                            </div>
-                            <div class="valid-feedback">
-                                Email valid !
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="password" class="col-sm-3 col-form-label">Password</label>
-                        <div class="col-sm-9">
-                            <input type="password" class="form-control" id="password" name="password" required>
-                            <div class="valid-feedback">
-                                Password valid !
-                            </div>
-                            <div class="invalid-feedback">
-                                Password tidak valid !
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group mb-0 row">
-                        <label for="password_confirmation" class="col-sm-3 col-form-label">Konfirmasi Password</label>
-                        <div class="col-sm-9">
-                            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
-                            <div class="valid-feedback">
-                                Konfirmasi password valid !
-                            </div>
-                            <div class="invalid-feedback">
-                                Konfirmasi password tidak sesuai !
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-                    <button type="submit" class="btn btn-primary">Tambah</button>
-                </div>
-            </form>
+                    <div class="col-md-4">
+                <button class="btn btn-primary" data-toggle="modal" data-target="#tambahModal">Tambah</button>
+            </div>
         </div>
-    </div>
-</div>
+          <div class="modal fade" id="tambahModal" tabindex="-1" aria-labelledby="tambahModalLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                  <div class="modal-content">
+                      <form class="needs-validation" novalidate="">
+                          <div class="modal-header">
+                              <h5 class="modal-title" id="tambahModalLabel">Form Tambah Anggota</h5>
+                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                  <span aria-hidden="true">&times;</span>
+                              </button>
+                          </div>
+                          <div class="modal-body">
+                              <div class="form-group row">
+                                  <label for="nama" class="col-sm-3 col-form-label">Nama</label>
+                                  <div class="col-sm-9">
+                                      <input type="text" class="form-control" id="nama" name="nama" required>
+                                      <div class="invalid-feedback">
+                                          Kolom wajib diisi !
+                                      </div>
+                                      <div class="valid-feedback">
+                                          Nama valid !
+                                      </div>
+                                  </div>
+                              </div>
+                              <div class="form-group row">
+                                  <label for="email" class="col-sm-3 col-form-label">Email</label>
+                                  <div class="col-sm-9">
+                                      <input type="email" class="form-control" id="email" name="email" required>
+                                      <div class="invalid-feedback">
+                                          Email tidak valid !
+                                      </div>
+                                      <div class="valid-feedback">
+                                          Email valid !
+                                      </div>
+                                  </div>
+                              </div>
+                              <div class="form-group row">
+                                  <label for="password" class="col-sm-3 col-form-label">Password</label>
+                                  <div class="col-sm-9">
+                                      <input type="password" class="form-control" id="password" name="password" required>
+                                      <div class="valid-feedback">
+                                          Password valid !
+                                      </div>
+                                      <div class="invalid-feedback">
+                                          Password tidak valid !
+                                      </div>
+                                  </div>
+                              </div>
+                              <div class="form-group mb-0 row">
+                                  <label for="password_confirmation" class="col-sm-3 col-form-label">Konfirmasi Password</label>
+                                  <div class="col-sm-9">
+                                      <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
+                                      <div class="valid-feedback">
+                                          Konfirmasi password valid !
+                                      </div>
+                                      <div class="invalid-feedback">
+                                          Konfirmasi password tidak sesuai !
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="modal-footer">
+                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                              <button type="submit" class="btn btn-primary">Tambah</button>
+                          </div>
+                      </form>
+                  </div>
+              </div>
+          </div>
 
           <div class="section-body">
           </div>
