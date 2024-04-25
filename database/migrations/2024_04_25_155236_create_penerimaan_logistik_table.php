d0872df8d5578d87475e09217166de9c7b571345
Ihsanuddin18 <mihsan18@gmail.com>
2024-04-25 23:06:46 +0700

    Penambahan: migrations penerimaan logistik

---
+++ database/migrations/2024_04_25_155236_create_penerimaan_logistik_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penerimaan_logistik', function (Blueprint $table) {
+            $table->id('id_penerimaan');
+            $table->string('nama_logistik');
+            $table->string('bantuan_dari');
+            $table->date('tgl_terima');
+            $table->integer('jumlah_terima');
+            $table->integer('satuan_terima');
+            $table->date('expayer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penerimaan_logistik');
+    }
+};
