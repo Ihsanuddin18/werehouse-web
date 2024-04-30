7f2701b68824395f1f1743744e1c7d8ed1fb7eaf
Ihsanuddin18 <mihsan18@gmail.com>
2024-04-30 13:47:50 +0700

    Perubahan: kolom fitur data logistik

--- resources/views/data_logistik.blade.php
+++ resources/views/data_logistik.blade.php
@@ -430,98 +430,59 @@
                         </tr>
                         <tr>
                           <td>1</td>
-                          <td>Create a mobile app</td>
-                          <td class="align-middle">
-                            <div class="progress" data-height="4" data-toggle="tooltip" title="100%">
-                              <div class="progress-bar bg-success" data-width="100"></div>
-                            </div>
-                          </td>
-                          <td>
-                            <img alt="image" src="{{ asset('tdashboard') }}/assets/img/avatar/avatar-5.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Wildan Ahdian">
-                          </td>
-                          <td>2018-01-20</td>
-                          <td><div class="badge badge-success">Completed</div></td>
+                          <td>09386437</td>
+                          <td>Kursi Lipat</td>
+                          <td>APBD Provinsi Jawa Timur</td>
+                          <td>14 Pcs</td>
+                          <td>2018-01-20</td> 
                           <td>
                             <a class="btn btn-primary btn-action mr-1" data-toggle="modal" data-target="#tambahModal" data-toggle="tooltip" title="Edit"><i class="fas fa-pencil-alt"></i></a>
                             <a class="btn btn-danger btn-action" data-toggle="tooltip" title="Delete" data-confirm="Apakah anda yakin?|Apakah anda yakin ingin menghapus Data Admin ini?" data-confirm-yes="alert('Deleted')"><i class="fas fa-trash"></i></a>
                           </td>
                         </tr>
                         <tr>
-                          <td>2</td>
-                          <td>Redesign homepage</td>
-                          <td class="align-middle">
-                            <div class="progress" data-height="4" data-toggle="tooltip" title="0%">
-                              <div class="progress-bar" data-width="0"></div>
-                            </div>
-                          </td>
-                          <td>
-                            <img alt="image" src="{{ asset('tdashboard') }}/assets/img/avatar/avatar-1.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Nur Alpiana">
-                            <img alt="image" src="{{ asset('tdashboard') }}/assets/img/avatar/avatar-3.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Hariono Yusup">
-                            <img alt="image" src="{{ asset('tdashboard') }}/assets/img/avatar/avatar-4.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Bagus Dwi Cahya">
-                          </td>
-                          <td>2018-04-10</td>
-                          <td><div class="badge badge-info">Todo</div></td>
+                          <td>1</td>
+                          <td>09386437</td>
+                          <td>Kursi Lipat</td>
+                          <td>APBD Provinsi Jawa Timur</td>
+                          <td>14 Pcs</td>
+                          <td>2018-01-20</td> 
                           <td>
                             <a class="btn btn-primary btn-action mr-1" data-toggle="modal" data-target="#tambahModal" data-toggle="tooltip" title="Edit"><i class="fas fa-pencil-alt"></i></a>
                             <a class="btn btn-danger btn-action" data-toggle="tooltip" title="Delete" data-confirm="Apakah anda yakin?|Apakah anda yakin ingin menghapus Data Admin ini?" data-confirm-yes="alert('Deleted')"><i class="fas fa-trash"></i></a>
                           </td>
                         </tr>
                         <tr>
-                          <td>3</td>
-                          <td>Backup database</td>
-                          <td class="align-middle">
-                            <div class="progress" data-height="4" data-toggle="tooltip" title="70%">
-                              <div class="progress-bar bg-warning" data-width="70"></div>
-                            </div>
-                          </td>
-                          <td>
-                            <img alt="image" src="{{ asset('tdashboard') }}/assets/img/avatar/avatar-1.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Rizal Fakhri">
-                            <img alt="image" src="{{ asset('tdashboard') }}/assets/img/avatar/avatar-2.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Hasan Basri">
-                          </td>
-                          <td>2018-01-29</td>
-                          <td><div class="badge badge-warning">In Progress</div></td>
+                          <td>1</td>
+                          <td>09386437</td>
+                          <td>Kursi Lipat</td>
+                          <td>APBD Provinsi Jawa Timur</td>
+                          <td>14 Pcs</td>
+                          <td>2018-01-20</td> 
                           <td>
                             <a class="btn btn-primary btn-action mr-1" data-toggle="modal" data-target="#tambahModal" data-toggle="tooltip" title="Edit"><i class="fas fa-pencil-alt"></i></a>
                             <a class="btn btn-danger btn-action" data-toggle="tooltip" title="Delete" data-confirm="Apakah anda yakin?|Apakah anda yakin ingin menghapus Data Admin ini?" data-confirm-yes="alert('Deleted')"><i class="fas fa-trash"></i></a>
                           </td>
                         </tr>
                         <tr>
-                          <td>4</td>
-                          <td>Input data</td>
-                          <td class="align-middle">
-                            <div class="progress" data-height="4" data-toggle="tooltip" title="100%">
-                              <div class="progress-bar bg-success" data-width="100"></div>
-                            </div>
-                          </td>
-                          <td>
-                            <img alt="image" src="{{ asset('tdashboard') }}/assets/img/avatar/avatar-2.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Rizal Fakhri">
-                            <img alt="image" src="{{ asset('tdashboard') }}/assets/img/avatar/avatar-5.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Isnap Kiswandi">
-                            <img alt="image" src="{{ asset('tdashboard') }}/assets/img/avatar/avatar-4.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Yudi Nawawi">
-                            <img alt="image" src="{{ asset('tdashboard') }}/assets/img/avatar/avatar-1.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Khaerul Anwar">
-                          </td>
-                          <td>2018-01-16</td>
-                          <td><div class="badge badge-success">Completed</div></td>
+                          <td>1</td>
+                          <td>09386437</td>
+                          <td>Kursi Lipat</td>
+                          <td>APBD Provinsi Jawa Timur</td>
+                          <td>14 Pcs</td>
+                          <td>2018-01-20</td> 
                           <td>
                             <a class="btn btn-primary btn-action mr-1" data-toggle="modal" data-target="#tambahModal" data-toggle="tooltip" title="Edit"><i class="fas fa-pencil-alt"></i></a>
                             <a class="btn btn-danger btn-action" data-toggle="tooltip" title="Delete" data-confirm="Apakah anda yakin?|Apakah anda yakin ingin menghapus Data Admin ini?" data-confirm-yes="alert('Deleted')"><i class="fas fa-trash"></i></a>
                           </td>
                         </tr>
                         <tr>
-                          <td>5</td>
-                          <td>Input data</td>
-                          <td class="align-middle">
-                            <div class="progress" data-height="4" data-toggle="tooltip" title="100%">
-                              <div class="progress-bar bg-success" data-width="100"></div>
-                            </div>
-                          </td>
-                          <td>
-                            <img alt="image" src="{{ asset('tdashboard') }}/assets/img/avatar/avatar-2.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Rizal Fakhri">
-                            <img alt="image" src="{{ asset('tdashboard') }}/assets/img/avatar/avatar-5.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Isnap Kiswandi">
-                            <img alt="image" src="{{ asset('tdashboard') }}/assets/img/avatar/avatar-4.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Yudi Nawawi">
-                            <img alt="image" src="{{ asset('tdashboard') }}/assets/img/avatar/avatar-1.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Khaerul Anwar">
-                          </td>
-                          <td>2018-01-16</td>
-                          <td><div class="badge badge-success">Completed</div></td>
+                          <td>1</td>
+                          <td>09386437</td>
+                          <td>Kursi Lipat</td>
+                          <td>APBD Provinsi Jawa Timur</td>
+                          <td>14 Pcs</td>
+                          <td>2018-01-20</td> 
                           <td>
                             <a class="btn btn-primary btn-action mr-1" data-toggle="modal" data-target="#tambahModal" data-toggle="tooltip" title="Edit"><i class="fas fa-pencil-alt"></i></a>
                             <a class="btn btn-danger btn-action" data-toggle="tooltip" title="Delete" data-confirm="Apakah anda yakin?|Apakah anda yakin ingin menghapus Data Admin ini?" data-confirm-yes="alert('Deleted')"><i class="fas fa-trash"></i></a>
@@ -539,7 +500,7 @@
       </div>
       <footer class="main-footer">
         <div class="footer-left">
-          Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+         Werehouse BPBD<div class="bullet"></div> Kabupaten Jember
         </div>
         <div class="footer-right">
         </div>
