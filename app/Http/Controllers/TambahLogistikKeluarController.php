49078554c90808be7b9860da6e27f34611d13e38
Ihsanuddin18 <mihsan18@gmail.com>
2024-05-02 14:18:17 +0700

    Penambahan: view tambah logistik masuk dan keluar

--- app/Http/Controllers/TambahLogistikKeluarController.php
+++ app/Http/Controllers/TambahLogistikKeluarController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class TambahLogistikKeluarController extends Controller
 {
-    //
+    public function showTambahLogistikKeluar()
+    {
+        return view('tambah_logistik_keluar');
+    }
 }
--- app/Http/Controllers/TambahLogistikMasukController.php
+++ app/Http/Controllers/TambahLogistikMasukController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class TambahLogistikMasukController extends Controller
 {
-    //
+    public function showTambahLogistikMasuk()
+    {
+        return view('tambah_logistik_masuk');
+    }
 }
