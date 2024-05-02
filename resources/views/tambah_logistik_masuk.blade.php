72f3c9e10aa2c55e4e35c93e1a163cf297e42faf
Ihsanuddin18 <mihsan18@gmail.com>
2024-05-02 20:30:24 +0700

    Perubahan dan penambahan: kolom fitur tambah logistik masuk

--- resources/views/tambah_logistik_masuk.blade.php
+++ resources/views/tambah_logistik_masuk.blade.php
@@ -434,63 +434,46 @@
           </div>
           <div class="card">
                   <div class="card-header">
-                    <h4>Data Admin</h4>
+                    <h4>Form Tambah Logistik Masuk</h4>
                   </div>
                   <div class="card-body">
                   <div class="form-row">
                   <div class="form-group col-md-3"> <!-- Bagian Baru untuk Kolom Telepon -->
-                            <label for="inputPhone">Phone</label>
+                            <label for="inputPhone">1</label>
                             <input type="tel" class="form-control" id="inputPhone" placeholder="Phone">
                         </div>
                         <div class="form-group col-md-3"> <!-- Bagian Baru untuk Kolom Telepon -->
-                            <label for="inputPhone">Phone</label>
-                            <input type="tel" class="form-control" id="inputPhone" placeholder="Phone">
-                        </div>
-                        <div class="form-group col-md-3"> <!-- Bagian Baru untuk Kolom Telepon -->
-                            <label for="inputPhone">Phone</label>
+                            <label for="inputPhone">2</label>
                             <input type="tel" class="form-control" id="inputPhone" placeholder="Phone">
                         </div>
                         <div class="form-group col-md-3"> <!-- Bagian Kolom Password -->
-                            <label for="inputPassword4">Password</label>
+                            <label for="inputPassword4">3</label>
+                            <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                    </div>
+                        <div class="form-group col-md-3"> <!-- Bagian Kolom Password -->
+                            <label for="inputPassword4">3</label>
                             <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
                     </div>
                       <div class="form-group col-md-6">
                         <label for="inputPassword4">Password</label>
                         <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
                       </div>
+                      <div class="form-group col-md-6"> <!-- Bagian Baru untuk Kolom Telepon -->
+                            <label for="inputPhone">1</label>
+                            <input type="tel" class="form-control" id="inputPhone" placeholder="Phone">
+                        </div>
+                      <div class="form-group col-md-6"> <!-- Bagian Baru untuk Kolom Telepon -->
+                            <label for="inputPhone">2</label>
+                            <input type="tel" class="form-control" id="inputPhone" placeholder="Phone">
+                        </div>
+                      <div class="form-group col-md-6"> <!-- Bagian Baru untuk Kolom Telepon -->
+                            <label for="inputPhone">3</label>
+                            <input type="tel" class="form-control" id="inputPhone" placeholder="Phone">
+                        </div>
                     </div>
                     <div class="form-group">
                       <label for="inputAddress">Address</label>
                       <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                    </div>
-                    <div class="form-group">
-                      <label for="inputAddress2">Address 2</label>
-                      <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-                    </div>
-                    <div class="form-row">
-                      <div class="form-group col-md-6">
-                        <label for="inputCity">City</label>
-                        <input type="text" class="form-control" id="inputCity">
-                      </div>
-                      <div class="form-group col-md-4">
-                        <label for="inputState">State</label>
-                        <select id="inputState" class="form-control">
-                          <option selected>Choose...</option>
-                          <option>...</option>
-                        </select>
-                      </div>
-                      <div class="form-group col-md-2">
-                        <label for="inputZip">Zip</label>
-                        <input type="text" class="form-control" id="inputZip">
-                      </div>
-                    </div>
-                    <div class="form-group mb-0">
-                      <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="gridCheck">
-                        <label class="form-check-label" for="gridCheck">
-                          Check me out
-                        </label>
-                      </div>
                     </div>
                   </div>
                   <div class="card-footer">
