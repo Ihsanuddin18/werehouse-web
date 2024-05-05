b4bfe33ea0c2d0af14b4722ac4fc940e6d92078a
Ihsanuddin18 <mihsan18@gmail.com>
2024-05-05 21:07:40 +0700

    Perubahan: fitur logistik masuk dan keluar

--- resources/views/logistik_masuk.blade.php
+++ resources/views/logistik_masuk.blade.php
@@ -117,138 +117,56 @@
       <div class="main-sidebar sidebar-style-2">
         <aside id="sidebar-wrapper">
           <div class="sidebar-brand">
-            <a href="index.html">Stisla</a>
+              <img alt="image" src="{{ asset('tdashboard') }}/assets/img/avatar/logobpbd1.png" style="width: 143px; height: auto; margin-top: 20px;">
+              <a href="{{ route('dashboard') }}"> Werehouse BPBD </a>
+              <hr style="margin-top: 3px; margin-bottom: 3px; border: none; border-bottom: 0.1px solid #C1C1C1; width: 80%;">
+              <p><br></p>
           </div>
           <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">St</a>
+            <a href="index.html">WB</a>
           </div>
           <ul class="sidebar-menu">
-            <li class="menu-header">Dashboard</li>
+              <li>
+               <a href="{{ route('dashboard') }}"><i class="fas fa-home"></i><span>Dashboard</span></a>
+              </li>
+            <li class="menu-header">Master</li>
             <li class="dropdown">
-              <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-              <ul class="dropdown-menu">
-                <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
-                <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
-              </ul>
-            </li>
-            <li class="menu-header">Data</li>
-            <li class="dropdown">
-              <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Data Logistik</span></a>
-              <ul class="dropdown-menu">
-                <li><a class="nav-link" href="{{ route('data_logistik') }}">Stok Logistik</a></li>
-                <li><a class="nav-link" href="{{ route('tambah_data') }}">Tambah Data</a></li>
-              </ul>
+              <a href="{{ route('data_logistik') }}"><i class="fas fa-database"></i> <span>Data Logistik</span></a>
             </li>
             <li class="dropdown">
-              <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>
-              <ul class="dropdown-menu">
-                <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
-                <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
-                <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
-              </ul>
+               <a href="{{ route('data_supplier') }}"><i class="fas fa-table"></i> <span>Data Supplier</span></a>
             </li>
-            <li ><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
-            <li class="menu-header">Transaksi</li>
-              <li class=active>
-                <a href="{{ route('logistik_masuk')}}" class="nav-link"><i class="far fa-file-alt"></i> <span>Logistik Masuk</span></a>
+            <li class="dropdown">
+              <a href="{{ route('tambah_akun_anggota')}}"><i class="far fa-user"></i> <span>Tambah Anggota</span></a>
+            </li>
+            <li class="menu-header">Aktivitas</li>
+              <li  class=active>
+                <a href="{{ route('logistik_masuk')}}" class="nav-link"><i class="fas fa-sign-in-alt"></i> <span>Logistik Masuk</span></a>
               </li>
               <li>
-                <a href="{{ route('logistik_keluar')}}" class="nav-link"><i class="far fa-file-alt"></i> <span>Logistik Keluar</span></a>
+                <a href="{{ route('logistik_keluar')}}" class="nav-link"><i class="fas fa-sign-out-alt"></i> <span>Logistik Keluar</span></a>
             </li>
-            <li class="menu-header">Stisla</li>
-            <li class="dropdown">
-              <a href="#" class="nav-link has-dropdown"><i class="fas fa-th-large"></i> <span>Components</span></a>
-              <ul class="dropdown-menu">
-                <li><a class="nav-link" href="components-article.html">Article</a></li>                <li><a class="nav-link beep beep-sidebar" href="components-avatar.html">Avatar</a></li>                <li><a class="nav-link" href="components-chat-box.html">Chat Box</a></li>                <li><a class="nav-link beep beep-sidebar" href="components-empty-state.html">Empty State</a></li>                <li><a class="nav-link" href="components-gallery.html">Gallery</a></li>
-                <li><a class="nav-link beep beep-sidebar" href="components-hero.html">Hero</a></li>                <li><a class="nav-link" href="components-multiple-upload.html">Multiple Upload</a></li>
-                <li><a class="nav-link beep beep-sidebar" href="components-pricing.html">Pricing</a></li>                <li><a class="nav-link" href="components-statistic.html">Statistic</a></li>                <li><a class="nav-link" href="components-tab.html">Tab</a></li>
-                <li><a class="nav-link" href="components-table.html">Table</a></li>
-                <li><a class="nav-link" href="components-user.html">User</a></li>                <li><a class="nav-link beep beep-sidebar" href="components-wizard.html">Wizard</a></li>              </ul>
+              <li>
+                <a href="{{ route('lokasi_pengiriman')}}" class="nav-link"><i class="fas fa-map-marker-alt"></i> <span>Lokasi Pengiriman</span></a>
+              </li>
+            <li class="menu-header">Pengaturan</li>
+              <li>
+                <a href="{{ route('profile.edit')}}" class="nav-link"><i class="fas fa-user"></i> <span>Profil</span></a>
+              </li>
+              <li>
+                <a href="{{ route('pengaturan') }}" class="nav-link"><i class="fas fa-cog"></i> <span>Pengaturan</span></a>
             </li>
-            <li class="dropdown">
-              <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i> <span>Forms</span></a>
-              <ul class="dropdown-menu">
-                <li><a class="nav-link" href="forms-advanced-form.html">Advanced Form</a></li>
-                <li><a class="nav-link" href="forms-editor.html">Editor</a></li>
-                <li><a class="nav-link" href="forms-validation.html">Validation</a></li>
-              </ul>
-            </li>
-            <li class="dropdown">
-              <a href="#" class="nav-link has-dropdown"><i class="fas fa-map-marker-alt"></i> <span>Google Maps</span></a>
-              <ul class="dropdown-menu">
-                <li><a href="gmaps-advanced-route.html">Advanced Route</a></li>
-                <li><a href="gmaps-draggable-marker.html">Draggable Marker</a></li>
-                <li><a href="gmaps-geocoding.html">Geocoding</a></li>
-                <li><a href="gmaps-geolocation.html">Geolocation</a></li>
-                <li><a href="gmaps-marker.html">Marker</a></li>
-                <li><a href="gmaps-multiple-marker.html">Multiple Marker</a></li>
-                <li><a href="gmaps-route.html">Route</a></li>
-                <li><a href="gmaps-simple.html">Simple</a></li>
-              </ul>
-            </li>            <li class="dropdown">
-              <a href="#" class="nav-link has-dropdown"><i class="fas fa-plug"></i> <span>Modules</span></a>
-              <ul class="dropdown-menu">
-                <li><a class="nav-link" href="modules-calendar.html">Calendar</a></li>
-                <li><a class="nav-link" href="modules-chartjs.html">ChartJS</a></li>
-                <li><a class="nav-link" href="modules-datatables.html">DataTables</a></li>
-                <li><a class="nav-link" href="modules-flag.html">Flag</a></li>
-                <li><a class="nav-link" href="modules-font-awesome.html">Font Awesome</a></li>
-                <li><a class="nav-link" href="modules-ion-icons.html">Ion Icons</a></li>
-                <li><a class="nav-link" href="modules-owl-carousel.html">Owl Carousel</a></li>
-                <li><a class="nav-link" href="modules-sparkline.html">Sparkline</a></li>
-                <li><a class="nav-link" href="modules-sweet-alert.html">Sweet Alert</a></li>
-                <li><a class="nav-link" href="modules-toastr.html">Toastr</a></li>
-                <li><a class="nav-link" href="modules-vector-map.html">Vector Map</a></li>
-                <li><a class="nav-link" href="modules-weather-icon.html">Weather Icon</a></li>
-              </ul>
-            </li>
-            <li class="menu-header">Pages</li>
-            <li class="dropdown">
-              <a href="#" class="nav-link has-dropdown"><i class="far fa-user"></i> <span>Auth</span></a>
-              <ul class="dropdown-menu">
-                <li><a href="auth-forgot-password.html">Forgot Password</a></li> 
-                <li><a href="auth-login.html">Login</a></li> 
-                <li><a href="auth-register.html">Register</a></li> 
-                <li><a href="auth-reset-password.html">Reset Password</a></li> 
-              </ul>
-            </li>
-            <li class="dropdown">
-              <a href="#" class="nav-link has-dropdown"><i class="fas fa-exclamation"></i> <span>Errors</span></a>
-              <ul class="dropdown-menu">
-                <li><a class="nav-link" href="errors-503.html">503</a></li> 
-                <li><a class="nav-link" href="errors-403.html">403</a></li> 
-                <li><a class="nav-link" href="errors-404.html">404</a></li> 
-                <li><a class="nav-link" href="errors-500.html">500</a></li> 
-              </ul>
-            </li>
-            <li class="dropdown">
-              <a href="#" class="nav-link has-dropdown"><i class="fas fa-bicycle"></i> <span>Features</span></a>
-              <ul class="dropdown-menu">
-                <li><a class="nav-link" href="features-activities.html">Activities</a></li>
-                <li><a class="nav-link" href="features-post-create.html">Post Create</a></li>
-                <li><a class="nav-link" href="features-posts.html">Posts</a></li>
-                <li><a class="nav-link" href="features-profile.html">Profile</a></li>
-                <li><a class="nav-link" href="features-settings.html">Settings</a></li>
-                <li><a class="nav-link" href="features-setting-detail.html">Setting Detail</a></li>
-                <li><a class="nav-link" href="features-tickets.html">Tickets</a></li>
-              </ul>
-            </li>
-            <li class="dropdown">
-              <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i> <span>Utilities</span></a>
-              <ul class="dropdown-menu">
-                <li><a href="utilities-contact.html">Contact</a></li>
-                <li><a class="nav-link" href="utilities-invoice.html">Invoice</a></li>
-                <li><a href="utilities-subscribe.html">Subscribe</a></li>
-              </ul>
-            </li>            <li><a class="nav-link" href="credits.html"><i class="fas fa-pencil-ruler"></i> <span>Credits</span></a></li>
-          </ul>
-
-          <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-            <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-              <i class="fas fa-rocket"></i> Documentation
-            </a>
-          </div>        </aside>
+            <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+                <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                    <button class="btn btn-primary btn-lg btn-block btn-icon-split" type="submit">
+                        <i class="fas fa-door-open"></i> Keluar
+                    </button>
+                </form>
+            </div>       
+        </aside>
       </div>
+
 
       <!-- Main Content -->
       <div class="main-content">
@@ -281,12 +199,6 @@
                     <h4>Daftar Logistik Masuk</h4>
                     <div class="card-header-form">
                       <form>
-                        <div class="input-group">
-                          <input type="text" class="form-control" placeholder="Search">
-                          <div class="input-group-btn">
-                            <button class="btn btn-primary"><i class="fas fa-search"></i></button>
-                          </div>
-                        </div>
                       </form>
                     </div>
                   </div>
@@ -312,7 +224,7 @@
                           <td>2018-01-20</td> 
                           <td>
                             <a class="btn btn-primary btn-action mr-1" data-toggle="modal" data-target="#tambahModal" data-toggle="tooltip" title="Edit"><i class="fas fa-pencil-alt"></i> Edit </a>
-                            <a class="btn btn-danger btn-action" data-toggle="tooltip" title="Delete" data-confirm="Apakah anda yakin?|Apakah anda yakin ingin menghapus Data Logistik ini?" data-confirm-yes="alert('Deleted')"><i class="fas fa-trash"></i> Hapus </a>
+                            <a class="btn btn-danger btn-action" data-toggle="tooltip" title="Delete" data-confirm="Apakah anda yakin?|Apakah anda yakin ingin menghapus Data ini?" data-confirm-yes="alert('Deleted')"><i class="fas fa-trash"></i> Hapus </a>
                           </td>
                         </tr>
                         <tr>
@@ -438,7 +350,7 @@
                     <div class="form-group row">
                         <label for="nama_logistik" class="col-sm-3 col-form-label">Nama Logistik</label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="nama_logistik" name="nama_logistik" value="Kursi Lipat" readonly>
+                            <input type="text" class="form-control" id="nama_logistik" name="nama_logistik" value="Kursi Lipat" required>
                             <div class="invalid-feedback">
                                 Kolom wajib diisi!
                             </div>
@@ -479,21 +391,9 @@
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="nama_admin" class="col-sm-3 col-form-label">Nama Admin</label>
-                        <div class="col-sm-9">
-                            <input type="text" class="form-control" id="nama_admin" name="nama_admin" value="Werehouse BPBD" readonly>
-                            <div class="invalid-feedback">
-                                Kolom wajib diisi!
-                            </div>
-                            <div class="valid-feedback">
-                                Valid!
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group row">
                         <label for="nama_supplier" class="col-sm-3 col-form-label">Nama Supplier</label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="nama_supplier" name="nama_supplier" value="APBD Provinsi Jawa Timur" readonly>
+                            <input type="text" class="form-control" id="nama_supplier" name="nama_supplier" value="APBD Provinsi Jawa Timur" required>
                             <div class="invalid-feedback">
                                 Kolom wajib diisi!
                             </div>
@@ -517,7 +417,7 @@
                     <div class="form-group row">
                         <label for="expayer" class="col-sm-3 col-form-label">Expayer</label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="expayer" name="expayer" value="2024-05-02" readonly>
+                            <input type="text" class="form-control" id="expayer" name="expayer" value="2024-05-02" required>
                             <!-- Ganti nilai value dengan tanggal yang sesuai jika diperlukan -->
                             <div class="invalid-feedback">
                                 Kolom wajib diisi!
