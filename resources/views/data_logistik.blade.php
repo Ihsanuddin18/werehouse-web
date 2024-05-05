1259d7c1ae77bd3cd3711d554004c0e20e2e9213
Ihsanuddin18 <mihsan18@gmail.com>
2024-05-05 13:44:31 +0700

    Perubahan: nav-menu fitur data logistik

--- resources/views/data_logistik.blade.php
+++ resources/views/data_logistik.blade.php
@@ -282,30 +282,40 @@
           </div>
           <ul class="sidebar-menu">
               <li>
-               <a href="#"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+               <a href="#"><i class="fas fa-home"></i><span>Dashboard</span></a>
               </li>
             <li class="menu-header">Master</li>
             <li class=active class="dropdown">
-              <a href="{{ route('data_logistik') }}"><i class="fas fa-th"></i> <span>Data Logistik</span></a>
+              <a href="{{ route('data_logistik') }}"><i class="fas fa-database"></i> <span>Data Logistik</span></a>
             </li>
             <li class="dropdown">
-               <a href="{{ route('data_supplier') }}"><i class="fas fa-columns"></i> <span>Data Supplier</span></a>
+               <a href="{{ route('data_supplier') }}"><i class="fas fa-table"></i> <span>Data Supplier</span></a>
             </li>
             <li class="dropdown">
               <a href="{{ route('tambah_akun_anggota')}}"><i class="far fa-user"></i> <span>Tambah Anggota</span></a>
             </li>
             <li class="menu-header">Aktivitas</li>
               <li>
-                <a href="{{ route('logistik_masuk')}}" class="nav-link"><i class="far fa-file-alt"></i> <span>Logistik Masuk</span></a>
+                <a href="{{ route('logistik_masuk')}}" class="nav-link"><i class="fas fa-sign-in-alt"></i> <span>Logistik Masuk</span></a>
               </li>
               <li>
-                <a href="{{ route('logistik_keluar')}}" class="nav-link"><i class="far fa-file-alt"></i> <span>Logistik Keluar</span></a>
+                <a href="{{ route('logistik_keluar')}}" class="nav-link"><i class="fas fa-sign-out-alt"></i> <span>Logistik Keluar</span></a>
+            </li>
+              <li>
+                <a href="{{ route('lokasi_pengiriman')}}" class="nav-link"><i class="fas fa-map-marker-alt"></i> <span>Lokasi Pengiriman</span></a>
+              </li>
+            <li class="menu-header">Pengaturan</li>
+              <li>
+                <a href="{{ route('profile.edit')}}" class="nav-link"><i class="fas fa-user"></i> <span>Profil</span></a>
+              </li>
+              <li>
+                <a href="{{ route('pengaturan') }}" class="nav-link"><i class="fas fa-cog"></i> <span>Pengaturan</span></a>
             </li>
             <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
                 <form method="POST" action="{{ route('logout') }}">
                                 @csrf
                     <button class="btn btn-primary btn-lg btn-block btn-icon-split" type="submit">
-                        <i class="fas fa-rocket"></i> Logout
+                        <i class="fas fa-door-open"></i> Keluar
                     </button>
                 </form>
             </div>       
