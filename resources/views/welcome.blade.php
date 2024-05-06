b2f39760066187e949a67620928f197b2d0c613f
Ihsanuddin18 <mihsan18@gmail.com>
2024-05-06 11:11:46 +0700

    Penambahan: icon fitur landingpages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,15 +86,15 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-          <h1>Better Solutions For Your Business</h1>
-          <h2>We are team of talented designers making websites with Bootstrap</h2>
+          <h1>Selamat Datang Di Platform Digital</h1>
+          <h2>Sistem informasi manajemen inventori logistik Werehouse BPBD Kabupaten Jember</h2>
           <div class="d-flex justify-content-center justify-content-lg-start">
-            <a href="#about" class="btn-get-started scrollto">Get Started</a>
-            <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+            <a href="" class="btn-get-started scrollto">Dapatkan Aplikasi</a>
+            <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Lihat Tutorial</span></a>
           </div>
         </div>
         <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-          <img src="{{ asset('tlandingpages') }}/assets/img/hero-img.png" class="img-fluid animated" alt="">
+          <img src="{{ asset('tlandingpages') }}/assets/img/hero-img-2.png" class="img-fluid animated" alt="">
         </div>
       </div>
     </div>
